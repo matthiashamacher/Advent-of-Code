b9355fced9bbe8b4a028e0e5a24ced19a78718c7
Matthias Hamacher <mhamacher@mhamacher.dev>
2022-12-02 16:51:56 +0100

    Add stats command

--- README.md
+++ README.md
@@ -16,7 +16,9 @@ If only one part should be executed the following options can be used:
 Part one: `-1` or `--partOne`  
 Part two: `-2` or `--partTwo`
 
-If the command should use the example input the `-t` or `--test` option can be used.
+If the command should use the example input the `-t` or `--test` option can be used.  
+If the stats should be saved the `-c` or `--commit` option can be used.  
+If the retries should be increased the `-r` or `--retries` option can be used.
 
 ## Creating a new day
 
@@ -25,3 +27,11 @@ defines the day of the puzzle.
 
 Optionally a date can be provided as second argument. Then that date will be used as the date of the puzzle otherwise the 
 current date will be used.
+
+
+## Statistics
+
+| Year | Day | Execution Time<br/>Part 1 | Memory Usage<br/>Part 1 | Time to solve<br/>Part 1 | Retries<br/>Part 1 | Execution Time<br/>Part 2 | Memory Usage<br/>Part 2 | Time to solve<br/>Part 2 | Retries<br/>Part 1 |
+|------|-----|---------------------------|-------------------------|--------------------------|--------------------|---------------------------|-------------------------|--------------------------|--------------------|
+| 2022 | 1   | 0 ms                      | 0 MB                    | 0 m                      | 0                  | 0 ms                      | 8 MB                    | 0 m                      | 1                  |
+| 2022 | 2   | 0 ms                      | 20 MB                   | 7 m                      | 0                  | 0 ms                      | 8 MB                    | 10 m                     | 0                  |
---
+++ src/Command/StatsCommand.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Command;
+
+use DateTimeImmutable;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Filesystem\Filesystem;
+
+#[AsCommand('app:stats', 'Calculate time needed and update README.md')]
+class StatsCommand extends Command
+{
+    protected function configure()
+    {
+        $this->addArgument(
+            'day',
+            InputArgument::REQUIRED,
+            'Day to calculate',
+        )->addArgument(
+            'year',
+            InputArgument::REQUIRED,
+            'Year to calculate',
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+        $io->title('Stats calculation');
+
+        $day  = $input->getArgument('day');
+        $year = $input->getArgument('year');
+
+        $io->info('Calculating day ' . $day . ' of ' . $year);
+
+        $file = file_get_contents(
+            sprintf(
+                '%s/../../stats/%s/day%s.json',
+                __DIR__,
+                $year,
+                $day
+            )
+        );
+
+        if ($file === false) {
+            $io->error('File not found');
+
+            return Command::FAILURE;
+        }
+
+        $data = json_decode($file, true);
+
+        $startTime = $data['start'];
+
+        if ($data['partOne']['end'] !== 0) {
+            $endPartOne = $data['partOne']['end'];
+
+            $data['timeNeeded']['partOne'] = ($endPartOne - $startTime);
+        }
+
+        if ($data['partTwo']['end'] !== 0) {
+            $endPartTwo = $data['partTwo']['end'];
+
+            $data['timeNeeded']['partTwo'] = (($endPartTwo - $startTime) - $data['timeNeeded']['partOne']);
+        }
+
+        $fileSystem = new Filesystem();
+        $fileSystem->dumpFile(
+            sprintf(
+                '%s/../../stats/%s/day%s.json',
+                __DIR__,
+                $year,
+                $day
+            ),
+            json_encode($data, JSON_PRETTY_PRINT)
+        );
+
+        $statsMarkdown = sprintf(
+            '| %d | %d | %s ms | %s MB | %s m | %s ms | %s MB | %s m |
+',
+            $year,
+            $day,
+            $data['partOne']['executionTime'],
+            (int) (($data['partOne']['memoryUsage'] / 1024) / 1024),
+            (int) ($data['timeNeeded']['partOne'] / 60),
+            $data['partTwo']['executionTime'],
+            (int) (($data['partTwo']['memoryUsage'] / 1024) / 1024),
+            (int) ($data['timeNeeded']['partTwo'] / 60)
+        );
+
+
+        $readMe = sprintf(
+            '%s/../../README.md',
+            __DIR__
+        );
+
+        $fileSystem->appendToFile($readMe, $statsMarkdown);
+
+        $io->success('Stats calculated');
+
+        return Command::SUCCESS;
+    }
+}
